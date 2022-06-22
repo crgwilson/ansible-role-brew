3f48924acd19adf989e24c9ef2ec115d0a4d1aaf
Craig Wilson <craigjwilson93@gmail.com>
2022-06-21 21:49:26 -0700

    Repo init

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,7 @@
+def test_default_files(host):
+    pass
+    # f = host.file("/opt/nvim-linux64/bin/nvim")
+    # assert f.is_file
+    #
+    # c = host.run("/opt/nvim-linux64/bin/nvim --version")
+    # assert "NVIM v0.7.0" in c.stdout
