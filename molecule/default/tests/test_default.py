ceb9903760115f9994cb77eccd10183856de5036
Craig Wilson <craigjwilson93@gmail.com>
2022-07-05 11:45:42 -0700

    Fix brew install prefix not being overridable

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,6 +1,6 @@
 def test_default_files(host):
-    f = host.file("/opt/brew-3.5.2/bin/brew")
+    f = host.file("/usr/local/brew-3.5.2/bin/brew")
     assert f.is_file
 
-    c = host.run("/opt/brew-3.5.2/bin/brew --version")
+    c = host.run("/usr/local/brew-3.5.2/bin/brew --version")
     assert "(shallow or no git repository)" in c.stdout
